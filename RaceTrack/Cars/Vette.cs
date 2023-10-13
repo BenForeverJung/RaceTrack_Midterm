81a91eff62e019e3a504f3078813741da6f75cf8
BenjaminJung <ben.jung2@my.wctc.edu>
2023-10-12 19:51:14 -0500

    Add project files.

---
+++ RaceTrack/Cars/Vette.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Vette : RaceCar
+    {
+        public Vette()
+        {
+            Name = "Vette";
+            TopSpeed = 125;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} engine starts");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} is all gas, no brakes!");
+            base.Brake();
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"The {Name} kills the ignition");
+        }
+    }
+}
